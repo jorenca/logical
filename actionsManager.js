fe1456efd3d276dee605540c9f1211b71c31e995
jorenca <jv_mw@abv.bg>
2015-08-26 19:01:45 +0300

    pages commit

---
+++ actionsManager.js
@@ -0,0 +1,95 @@
+var NO_ACTION = ['moveDelta', 0, 0];
+var MOV_LEFT = ['moveDelta', -90, 0];
+var MOV_RIGHT = ['moveDelta', 90, 0];
+var MOV_UP = ['moveDelta', 0, -90];
+var MOV_DOWN = ['moveDelta', 0, 90];
+
+var CREATE_AND = ['createEntity', 'and'];
+var CREATE_OR = ['createEntity', 'or'];
+var CREATE_NOT = ['createEntity', 'not'];
+var CREATE_XOR = ['createEntity', 'xor'];
+var CREATE_TRUE = ['createEntity', 'ctrue'];
+var CREATE_FALSE = ['createEntity', 'cfalse'];
+var CREATE_RESULT = ['createEntity', 'result'];
+
+var CONNECTION_BEGIN = ['startAttach'];
+var CONNECTION_END = ['endAttach'];
+var CONNREMOVAL_BEGIN = [];
+var CONNREMOVAL_END = [];
+
+var EVAL = ['evalLogic'];
+
+function ActionsManager(){
+  // This variable stores the currently selected action and action parameters
+	this.currentAction = NO_ACTION;
+	this.resetAction = function(){
+	  this.currentAction = NO_ACTION;
+	};
+	
+	// This method is invoked in the main file to load all option buttons
+	this.buttonSprites = function(){
+		var sprites = [];
+		
+		sprites.push(makeModificator(0, 0, 0, MOV_LEFT, false));
+		sprites.push(makeModificator(1, 0, 1, MOV_RIGHT, false));
+		sprites.push(makeModificator(2, 0, 2, MOV_UP, false));
+		sprites.push(makeModificator(3, 0, 3, MOV_DOWN, false));
+		
+		
+		sprites.push(makeModificator(4, 0, 4, CREATE_AND, true));
+		sprites.push(makeModificator(5, 0, 5, CREATE_OR, true));
+		sprites.push(makeModificator(6, 0, 6, CREATE_NOT, true));
+		sprites.push(makeModificator(7, 0, 7, CREATE_XOR, true));
+		sprites.push(makeModificator(9, 1, 0, CREATE_TRUE, true));
+		sprites.push(makeModificator(10, 1, 1, CREATE_FALSE, true));
+		sprites.push(makeModificator(12, 1, 2, CREATE_RESULT, true));
+		
+		
+		sprites.push(makeModificator(8, 0, 8, CONNECTION_BEGIN, false));
+		sprites.push(makeModificator(13, 1, 8, CONNREMOVAL_BEGIN, false));
+		
+		
+		sprites.push(makeModificator(11, 1, 3, EVAL, false));
+		
+		return sprites;
+	};
+	
+	// This method should be triggered by action buttons
+	this.runAction = function(){
+	  var action = ACTMAN.currentAction;
+	  switch(action[0]){
+	    case 'createEntity':
+	      console.log("Creating new entity...");
+	      var ent = new GraphicEntity(200, 200, action[1]);
+	      break;
+	    default: 
+	      console.log("Unknown action was supplied: "+action[0]);
+	      break;
+	  }
+	  if(action == ACTMAN.currentAction){
+	    ACTMAN.currentAction = NO_ACTION;
+	  }
+	};
+}
+
+function makeModificator(index, col, row, action, activating){
+	var button = getButton(index, col, row);
+	button.click = function(){
+	  ACTMAN.currentAction = action;
+	  if(activating){
+	    ACTMAN.runAction();
+	  }
+	};
+	return button;
+}
+
+// index is the sprite index
+function getButton(index, col, row){
+  var button = PIXI.Sprite.fromFrame(index);
+	button.buttonMode = true;
+	button.anchor.set(0.5);
+	button.position.x = 45+col*70;
+	button.position.y = 45+row*70;
+	button.interactive = true;
+	return button;
+}
---
+++ objCreator.js
@@ -0,0 +1,189 @@
+var ENTITY_ICON = {};
+ENTITY_ICON['and'] = 20;
+ENTITY_ICON['or'] = 21;
+ENTITY_ICON['not'] = 22;
+ENTITY_ICON['xor'] = 23;
+
+ENTITY_ICON['ctrue'] = 9;
+ENTITY_ICON['cfalse'] = 10;
+ENTITY_ICON['result'] = 30;
+
+var LOGIC_ENTITY_CLASS = {};
+LOGIC_ENTITY_CLASS['and'] = AndEntity;
+LOGIC_ENTITY_CLASS['or'] = OrEntity;
+
+LOGIC_ENTITY_CLASS['ctrue'] = TrueEntity;
+LOGIC_ENTITY_CLASS['cfalse'] = FalseEntity;
+LOGIC_ENTITY_CLASS['result'] = ResultEntity;
+
+
+var ALL_RESULT_ENTITIES = [];
+var RESULT_ENTITY_UNDEFINED_SPRITE = 30;
+var RESULT_ENTITY_TRUE_SPRITE = 31;
+var RESULT_ENTITY_FALSE_SPRITE = 32;
+function refreshResultEntities(){
+  console.log("Refreshing result sprites...");
+  for(var i=0; i<ALL_RESULT_ENTITIES.length; i++){
+    var resultEntity = ALL_RESULT_ENTITIES[i];
+	  var newValue = resultEntity.logicEntity.outputValue();
+	  var newSpriteId = (newValue === undefined) ? RESULT_ENTITY_UNDEFINED_SPRITE : (newValue ? RESULT_ENTITY_TRUE_SPRITE : RESULT_ENTITY_FALSE_SPRITE);
+	  resultEntity.drawableEntity.changeSpriteTo(newSpriteId);
+  }
+}
+
+var CURR_UNIQ_ID = 0;
+function GraphicEntity(x, y, logicEntity){
+  this.uniqueId = CURR_UNIQ_ID++;
+	this.logicEntity = new LOGIC_ENTITY_CLASS[logicEntity]();
+	this.drawableEntity = new DrawableEntity(x, y, ENTITY_ICON[logicEntity], this);
+	
+  this.attachedSrc = {};
+  this.attachedDst = {};
+  
+  if(logicEntity == 'result'){
+    ALL_RESULT_ENTITIES.push(this);
+  }
+	
+
+  // Movement action
+	this.moveDelta = function(dX, dY){
+		this.drawableEntity.moveDelta(dX, dY);
+	};
+	
+	// Attaching function
+	this.startAttach = function(){
+	  var startLogic =  this.logicEntity;
+	  if(!startLogic.canBeStart){
+	    alert("This node cannot be a connection startpoint.");
+	    ACTMAN.resetAction();
+	    return;
+	  }
+	  var actionEnd = [CONNECTION_END[0], this];
+	  ACTMAN.currentAction = actionEnd;
+	};
+	
+	// Accomplishing attachment function
+	this.endAttach = function(startEntity){
+	  ACTMAN.resetAction();
+	  
+	  if(startEntity === this){
+	    alert("Cannot attach to self.");
+	    return;
+	  }
+	  var endEntity = this;
+	  if(!endEntity.logicEntity.canBeEnd){
+	    alert("This node cannot be a connection endpoint.");
+	    return;
+	  }
+	  
+	  if(!endEntity.logicEntity.canAcceptMoreSources){
+	    alert("This node cannot accept any more logic sources.");
+	    return;
+	  }
+	  
+	  if(endEntity in startEntity.attachedDst){// FIXME remove debug output
+	    console.log("Between "); console.log(startEntity); console.log(this);
+	    alert("Such a connection already exists.");
+	    return;
+	  }
+	  
+	  startEntity.beSourceFor(endEntity);
+	  endEntity.beSourcedBy(startEntity);
+	  
+	  
+	  console.log("Attaching end");
+	  // renew status tiles
+	  refreshResultEntities();
+	};
+	
+	
+	this.evalLogic = function(){
+	  alert(this.logicEntity.outputValue());
+	};
+	
+	this.beSourceFor = function(endEntity){
+	  this.attachedDst[endEntity.uniqueId] = true;
+	  this.drawableEntity.attachTo(endEntity.drawableEntity);
+	};
+	this.beSourcedBy = function(sourceEntity){
+	  this.attachedSrc[sourceEntity.uniqueId] = true;
+	  this.logicEntity.addSource(sourceEntity.logicEntity);
+	};
+}
+
+function DrawableEntity(x, y, spriteFrame, listener){
+  this.graphics = PIXI.Sprite.fromFrame(spriteFrame);
+	this.graphics.hitArea = this.graphics.getBounds();
+	this.graphics.interactive = true;
+	this.graphics.position.x = x;
+	this.graphics.position.y = y;
+	stage.addChild(this.graphics);
+	
+	this.moveDelta = function(dX, dY){
+	  this.graphics.x += dX;
+		this.graphics.y += dY;
+		
+		for(var i = 0; i < this.linesTo.length; i++) {
+		  var line = this.linesTo[i];
+		  line.refresh();
+    }
+	};
+	
+	this.graphics.gEntity = listener;
+	this.graphics.click = function(){
+		var action = ACTMAN.currentAction;
+		console.log("Action to execute: ");
+		console.log(action);
+		this.tint = 0x888888;
+		this.gEntity[action[0]](action[1], action[2]);
+	};
+	
+	
+	this.linesTo = [];
+	this.attachTo = function(otherDrawable){
+	  var newLine = new Line(this, otherDrawable);
+	  this.linesTo.push(newLine);
+	  otherDrawable.linesTo.push(newLine);
+	};
+	
+	this.changeSpriteTo = function(spriteIndex){
+	  var oldGraphics = this.graphics;
+	  this.graphics = PIXI.Sprite.fromFrame(spriteIndex);
+	  this.graphics.hitArea = this.graphics.getBounds();
+	  this.graphics.interactive = true;
+	  this.graphics.position.x = oldGraphics.position.x;
+	  this.graphics.position.y = oldGraphics.position.y;
+	  this.graphics.gEntity = oldGraphics.gEntity;
+	  this.graphics.click = oldGraphics.click;
+	  
+	  stage.removeChild(oldGraphics);
+	  stage.addChild(this.graphics);
+	};
+}
+
+function Line(fromEntity, toEntity){
+  this.from = fromEntity;
+  this.to = toEntity;
+  this.graphics = undefined;
+ 
+ console.log("Creating connection:"); console.log(this.from); console.log(this.to);
+ 
+  this.destroy = function(){
+    stage.removeChild(graphics);
+  };
+  
+  this.refresh = function(){
+    if(this.graphics !== undefined){
+      stage.removeChild(this.graphics);
+    }
+    this.graphics = new PIXI.Graphics();
+    this.graphics.lineStyle(10, 0x222222, 1);
+    this.graphics.moveTo(this.from.graphics.position.x+60, this.from.graphics.position.y+32);
+    this.graphics.lineTo(this.to.graphics.position.x+4, this.to.graphics.position.y+32);
+    stage.addChild(this.graphics);
+  };
+  
+  this.refresh();
+}
+
+
